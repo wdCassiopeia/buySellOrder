aceff683116e85711c9b0048938b6375cf31861d
wdCassiopeia <ucakar.klavdija@gmail.com>
2021-06-15 21:25:45 +0200

    initial commit

---
+++ buySellOrder/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace buySellOrder
+{
+    public class OrderBookItem
+    {
+        public int Exchange { get; set; }
+        public string OrderType { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // error if amount is not int or negative
+
+            // error if type not buy or sell
+            Console.Write("Enter 'Buy' or 'Sell': ");
+            string type = Console.ReadLine();
+            Console.Write("Enter BTC amount: ");
+            string number = Console.ReadLine();
+            bestExecution(number, type);
+        }
+        public static void bestExecution(string number, string type)
+        {
+            decimal amount;
+            bool success = decimal.TryParse(number, out amount);
+            if (!success || amount <= 0)
+            {
+                throw new Exception("Amount is negative or in a wrong format");
+            }
+            if (type != "Buy" && type != "Sell")
+            {
+                throw new Exception("Type has to be one of the following: 'Buy', 'Sell'");
+            }
+            // error if amount is not int or negative
+
+            // error if type not buy or sell
+
+            string[] lines = System.IO.File.ReadAllLines("order_books_data.txt");
+            int lineNumber = 0;
+            List<OrderBookItem> orders = new List<OrderBookItem>();
+            foreach (string line in lines)
+            {
+                // the first column of line is a timestamp, and the second is the json with data
+                string[] parts = line.Split();
+                string limitsStr = parts[1];
+                dynamic limits = JsonConvert.DeserializeObject(limitsStr);
+                if (type == "Sell")
+                {
+                    foreach (dynamic bid in limits["Bids"])
+                    {
+                        OrderBookItem item = new OrderBookItem();
+                        item.Exchange = lineNumber;
+                        item.Amount = decimal.Parse(bid["Order"]["Amount"].ToString(), NumberStyles.Float);
+                        item.Price = decimal.Parse(bid["Order"]["Price"].ToString(), NumberStyles.Float);
+                        orders.Add(item);
+                    }
+                }
+                else
+                {
+                    foreach (dynamic ask in limits["Asks"])
+                    {
+                        OrderBookItem item = new OrderBookItem();
+                        item.Exchange = lineNumber;
+                        item.Amount = decimal.Parse(ask["Order"]["Amount"].ToString(), NumberStyles.Float);
+                        item.Price = decimal.Parse(ask["Order"]["Price"].ToString(), NumberStyles.Float);
+                        orders.Add(item);
+                    }
+
+                }
+                lineNumber += 1;
+            }
+            List<OrderBookItem> sortedList = new List<OrderBookItem>();
+            if (type == "Sell") {
+                // if selling, we want the price to be as high as possible
+                sortedList = orders.OrderByDescending(o => o.Price).ToList();
+            } else
+            { // if buying, we aim for lower prices
+                sortedList = orders.OrderBy(o => o.Price).ToList();
+            }
+
+            foreach(OrderBookItem trade in getItemsFromOrderBooks(amount, sortedList))
+            {
+                Console.WriteLine(type + " " + trade.Amount.ToString() + " on exchange " + trade.Exchange);
+
+            }
+     
+        }
+
+
+        public static List<OrderBookItem> getItemsFromOrderBooks(decimal amount, List<OrderBookItem> sortedList)
+        {
+            decimal remainingAmount = amount;
+            List<OrderBookItem> optimalTrades = new List<OrderBookItem>();
+            foreach (OrderBookItem item in sortedList)
+            {
+                if (item.Amount  >= remainingAmount)
+                {
+                    OrderBookItem partialAmountItem = item;
+                    partialAmountItem.Amount = remainingAmount;
+                    optimalTrades.Add(item);
+                    break;
+                } else
+                {
+                    optimalTrades.Add(item);
+                    remainingAmount -= item.Amount;
+                }
+            }
+            return optimalTrades;
+        }
+    }
+}
